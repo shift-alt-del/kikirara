4f8f3963ef32e43651cbae8a09c0a491725af197
w-ding <wding@confluent.io>
2022-11-28 11:42:15 +0900

    Add new endpoint

--- ha-debug.sh
+++ ha-debug.sh
@@ -3,13 +3,14 @@ while [ "$i" -ne 0 ]
 do
     echo "=== " ${i}
 
-    for id in {1..1000};
+    for id in {11..11};
     do
         echo "+++"
-        echo "ksql1:" $(curl -sX GET "http://localhost:8000/ksqldb?server=ksqldb-server&veh_id=${id}")
-        echo "ksql2:" $(curl -sX GET "http://localhost:8000/ksqldb?server=ksqldb-server2&veh_id=${id}")
-        echo "redis:" $(curl -sX GET "http://localhost:8000/redis?veh_id=${id}")
-        echo "pg   :" $(curl -sX GET "http://localhost:8000/pg?veh_id=${id}")
+        echo "ksql1   :" $(curl -sX GET "http://localhost:8000/ksqldb?server=ksqldb-server&veh_id=${id}")
+        echo "ksql2   :" $(curl -sX GET "http://localhost:8000/ksqldb?server=ksqldb-server2&veh_id=${id}")
+        echo "redis   :" $(curl -sX GET "http://localhost:8000/redis?veh_id=${id}")
+        echo "pg      :" $(curl -sX GET "http://localhost:8000/pg/bus_current_sr?veh_id=${id}")
+        echo "pg supp :" $(curl -sX GET "http://localhost:8000/pg/bus_current_sr_10s?veh_id=${id}")
     done
 
     i=$(($i+1))
